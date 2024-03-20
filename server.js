faaa68bbc4bb2e2bc915c9ee68db5f2f1927f7c4
Alex Truong <87406255+alextruonglt@users.noreply.github.com>
2024-03-19 18:24:31 -0700

    added job func

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+import "express-async-errors"
 import express from "express"
 import morgan from "morgan"
 import * as dotenv from "dotenv"
 import { nanoid } from "nanoid"
 import jobRouter from "./routes/jobRouter.js"
+import mongoose from "mongoose"
 
 dotenv.config()
 
@@ -16,7 +18,6 @@ app.use(express.json())
 
 app.post("/", (req, res) => {
   console.log(req)
-
   res.json({ message: "Data received", data: req.body })
 })
 
@@ -29,8 +30,14 @@ app.use((err, req, res, next) => {
   res.status(500).json({ msg: "something went wrong" })
 })
 
-const port = process.env.PORT || 510
-0
-app.listen(port, () => {
-  console.log(`server running on PORT ${port}....`)
-})
+const port = process.env.PORT || 5100
+
+try {
+  await mongoose.connect(process.env.MONGO_URL)
+  app.listen(port, () => {
+    console.log(`server running on PORT ${port}....`)
+  })
+} catch (error) {
+  console.log(error)
+  process.exit(1)
+}
