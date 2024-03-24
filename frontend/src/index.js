ed7df4ee492fe4bed759612d54f32a9689f3b630
Alex Truong <87406255+alextruonglt@users.noreply.github.com>
2024-03-24 12:16:41 -0700

    added backend connection

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,11 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./index.css"
+
+fetch("/api/v1/test")
+  .then((res) => res.json())
+  .then((data) => console.log(data))
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
