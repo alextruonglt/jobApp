a325d696ee8b25081a7f74b8de54d910f695ac09
Alex Truong <87406255+alextruonglt@users.noreply.github.com>
2024-03-27 14:01:43 -0700

    ready for first deployment

--- frontend/src/components/SearchContainer.js
+++ frontend/src/components/SearchContainer.js
@@ -11,6 +11,17 @@ const SearchContainer = () => {
   const { search, jobStatus, jobType, sort } = searchValues
 
   const submit = useSubmit()
+
+  const debounce = (onChange) => {
+    let timeout
+    return (e) => {
+      const form = e.currentTarget.form
+      clearTimeout(timeout)
+      timeout = setTimeout(() => {
+        onChange(form)
+      }, 1000)
+    }
+  }
   return (
     <Wrapper>
       <Form className="form">
@@ -22,9 +33,9 @@ const SearchContainer = () => {
             type="search"
             name="search"
             defaultValue={search}
-            onChange={(e) => {
-              submit(e.currentTarget.form)
-            }}
+            onChange={debounce((form) => {
+              submit(form)
+            })}
           />
           <FormRowSelect
             labelText="job status"
