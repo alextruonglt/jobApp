677d7a9062469e0d24eba07bbb2e3779adf53512
alextruonglt <alextruonglt@gmail.com>
2024-10-14 12:18:56 -0700

    search form update

--- frontend/src/components/FormRowSelect.js
+++ frontend/src/components/FormRowSelect.js
@@ -1,25 +1,31 @@
 const FormRowSelect = ({ name, labelText, list, defaultValue, onChange }) => {
-  return (
-    <div className="form-row">
-      <label htmlFor={name} className="form-label">
-        {labelText || name}
-      </label>
-      <select
-        name={name}
-        id={name}
-        className="form-select"
-        onChange={onChange}
-        defaultValue={defaultValue}
-      >
-        {list.map((itemValue) => {
-          return (
-            <option key={itemValue} value={itemValue}>
-              {itemValue}
-            </option>
-          )
-        })}
-      </select>
-    </div>
-  )
+	const toTitleCase = (str) => {
+		return str.replace(/\b\w/g, (char) => char.toUpperCase()).replace("-", " ")
+	}
+
+	return (
+		<div className="form-row">
+			<label htmlFor={name} className="form-label">
+				{labelText || name}
+			</label>
+			<select
+				name={name}
+				id={name}
+				className="form-select"
+				onChange={onChange}
+				defaultValue={defaultValue}
+			>
+				{list.map((itemValue) => {
+					return (
+						<option key={itemValue} value={itemValue}>
+							{toTitleCase(itemValue)}
+						</option>
+					)
+				})}
+			</select>
+		</div>
+	)
 }
+
+// Export the component at the bottom
 export default FormRowSelect
