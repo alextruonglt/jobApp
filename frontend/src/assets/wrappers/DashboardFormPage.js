d16d680841f981accb51ec299940ed437df4bb12
alextruonglt <alextruonglt@gmail.com>
2024-10-14 15:02:39 -0700

    added URL and description to add jobs

--- frontend/src/assets/wrappers/DashboardFormPage.js
+++ frontend/src/assets/wrappers/DashboardFormPage.js
@@ -1,46 +1,63 @@
-import styled from 'styled-components';
+import styled from "styled-components"
 
 const Wrapper = styled.section`
-  border-radius: var(--border-radius);
-  width: 100%;
-  background: var(--background-secondary-color);
-  padding: 3rem 2rem 4rem;
-  .form-title {
-    margin-bottom: 2rem;
-  }
-  .form {
-    margin: 0;
-    border-radius: 0;
-    box-shadow: none;
-    padding: 0;
-    max-width: 100%;
-    width: 100%;
-  }
-  .form-row {
-    margin-bottom: 0;
-  }
-  .form-center {
-    display: grid;
-    row-gap: 1rem;
-  }
-  .form-btn {
-    align-self: end;
-    margin-top: 1rem;
-    display: grid;
-    place-items: center;
-  }
-  @media (min-width: 992px) {
-    .form-center {
-      grid-template-columns: 1fr 1fr;
-      align-items: center;
-      column-gap: 1rem;
-    }
-  }
-  @media (min-width: 1120px) {
-    .form-center {
-      grid-template-columns: 1fr 1fr 1fr;
-    }
-  }
-`;
+	border-radius: var(--border-radius);
+	width: 100%;
+	background: var(--background-secondary-color);
+	padding: 3rem 2rem 4rem;
 
-export default Wrapper;
+	.form-title {
+		margin-bottom: 2rem;
+	}
+	.form {
+		margin: 0;
+		border-radius: 0;
+		box-shadow: none;
+		padding: 0;
+		max-width: 100%;
+		width: 100%;
+	}
+	.form-row {
+		margin-bottom: 0;
+	}
+	.form-center {
+		display: grid;
+		row-gap: 1rem;
+	}
+	.form-btn {
+		align-self: end;
+		margin-top: 1rem;
+		display: grid;
+		place-items: center;
+	}
+
+	/* Styles for textarea */
+	.form-textarea {
+		width: 100%;
+		resize: vertical; /* Allows vertical resizing */
+		padding: 0.5rem;
+		font-family: inherit;
+		font-size: inherit;
+		border-radius: var(--border-radius);
+		border: 1px solid var(--border-color);
+		background: var(--background-color);
+		min-height: 60px; /* Default height */
+		max-height: 300px; /* Limit the max height */
+	}
+
+	@media (min-width: 992px) {
+		.form-center {
+			grid-template-columns: 1fr 1fr;
+			align-items: center;
+			column-gap: 1rem;
+		}
+	}
+
+	@media (min-width: 1120px) {
+		.form-center {
+			grid-template-columns: 1fr 1fr 1fr;
+		}
+	}
+`
+
+export default Wrapper
--- frontend/src/pages/AddJob.js
+++ frontend/src/pages/AddJob.js
@@ -8,52 +8,58 @@ import customFetch from "../utils/customFetch.js"
 import FormRowSelect from "../components/FormRowSelect.js"
 import SubmitBtn from "../components/SubmitBtn.js"
 export const action = async ({ request }) => {
-  const formData = await request.formData()
-  const data = Object.fromEntries(formData)
+	const formData = await request.formData()
+	const data = Object.fromEntries(formData)
 
-  try {
-    await customFetch.post("/jobs", data)
-    toast.success("Job added successfully")
-    return redirect("all-jobs")
-  } catch (error) {
-    toast.error(error?.response?.data?.msg)
-    return error
-  }
+	try {
+		await customFetch.post("/jobs", data)
+		toast.success("Job added successfully")
+		return redirect("all-jobs")
+	} catch (error) {
+		toast.error(error?.response?.data?.msg)
+		return error
+	}
 }
 
 const AddJob = () => {
-  const { user } = useOutletContext()
+	const { user } = useOutletContext()
 
-  return (
-    <Wrapper>
-      <Form method="post" className="form">
-        <h4 className="form-title">add job</h4>
-        <div className="form-center">
-          <FormRow type="text" name="position" />
-          <FormRow type="text" name="company" />
-          <FormRow
-            type="text"
-            labelText="job location"
-            name="jobLocation"
-            defaultValue={user.location}
-          />
-          <FormRowSelect
-            labelText="job status"
-            name="jobStatus"
-            defaultValue={JOB_STATUS.PENDING}
-            list={Object.values(JOB_STATUS)}
-          />
-          <FormRowSelect
-            name="jobType"
-            labelText="job type"
-            defaultValue={JOB_TYPE.FULL_TIME}
-            list={Object.values(JOB_TYPE)}
-          />
-          <SubmitBtn formBtn />
-        </div>
-      </Form>
-    </Wrapper>
-  )
+	return (
+		<Wrapper>
+			<Form method="post" className="form">
+				<h4 className="form-title">add job</h4>
+				<div className="form-center">
+					<FormRow type="text" name="position" />
+					<FormRow type="text" name="company" />
+					<FormRow
+						type="text"
+						labelText="job location"
+						name="jobLocation"
+						defaultValue={user.location}
+					/>
+					<FormRowSelect
+						labelText="job status"
+						name="jobStatus"
+						defaultValue={JOB_STATUS.PENDING}
+						list={Object.values(JOB_STATUS)}
+					/>
+					<FormRowSelect
+						name="jobType"
+						labelText="job type"
+						defaultValue={JOB_TYPE.FULL_TIME}
+						list={Object.values(JOB_TYPE)}
+					/>
+					<FormRow type="text" name="jobURL" labelText="Job URL" />
+					<FormRow
+						type="textarea" // Use textarea for job description
+						name="jobDescription"
+						labelText="Job Description"
+					/>
+					<SubmitBtn formBtn />
+				</div>
+			</Form>
+		</Wrapper>
+	)
 }
 
 export default AddJob
