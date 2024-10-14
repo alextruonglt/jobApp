8f55ffdde1f36efa9c171727efb12639309ac9f2
alextruonglt <alextruonglt@gmail.com>
2024-10-14 15:18:35 -0700

    edit job updated

--- frontend/src/pages/EditJob.js
+++ frontend/src/pages/EditJob.js
@@ -9,62 +9,74 @@ import customFetch from "../utils/customFetch"
 import SubmitBtn from "../components/SubmitBtn"
 
 export const loader = async ({ params }) => {
-  try {
-    const { data } = await customFetch.get(`/jobs/${params.id}`)
-    return data
-  } catch (error) {
-    toast.error(error.response.data.msg)
-    return redirect("/dashboard/all-jobs")
-  }
+	try {
+		const { data } = await customFetch.get(`/jobs/${params.id}`)
+		return data
+	} catch (error) {
+		toast.error(error.response.data.msg)
+		return redirect("/dashboard/all-jobs")
+	}
 }
 
 export const action = async ({ request, params }) => {
-  const formData = await request.formData()
-  const data = Object.fromEntries(formData)
+	const formData = await request.formData()
+	const data = Object.fromEntries(formData)
 
-  try {
-    await customFetch.patch(`/jobs/${params.id}`, data)
-    toast.success("Job edited successfully")
-    return redirect("/dashboard/all-jobs")
-  } catch (error) {
-    toast.error(error.response.data.msg)
-    return error
-  }
+	try {
+		await customFetch.patch(`/jobs/${params.id}`, data)
+		toast.success("Job edited successfully")
+		return redirect("/dashboard/all-jobs")
+	} catch (error) {
+		toast.error(error.response.data.msg)
+		return error
+	}
 }
 
 const EditJob = () => {
-  const { job } = useLoaderData()
+	const { job } = useLoaderData()
 
-  return (
-    <Wrapper>
-      <Form method="post" className="form">
-        <h4 className="form-title">edit job</h4>
-        <div className="form-center">
-          <FormRow type="text" name="position" defaultValue={job.position} />
-          <FormRow type="text" name="company" defaultValue={job.company} />
-          <FormRow
-            type="text"
-            labelText="job location"
-            name="jobLocation"
-            defaultValue={job.jobLocation}
-          />
-          <FormRowSelect
-            name="jobStatus"
-            labelText="job status"
-            defaultValue={job.jobStatus}
-            list={Object.values(JOB_STATUS)}
-          />
-          <FormRowSelect
-            name="jobType"
-            labelText="job type"
-            defaultValue={job.jobType}
-            list={Object.values(JOB_TYPE)}
-          />
-          <SubmitBtn formBtn />
-        </div>
-      </Form>
-    </Wrapper>
-  )
+	return (
+		<Wrapper>
+			<Form method="post" className="form">
+				<h4 className="form-title">edit job</h4>
+				<div className="form-center">
+					<FormRow type="text" name="position" defaultValue={job.position} />
+					<FormRow type="text" name="company" defaultValue={job.company} />
+					<FormRow
+						type="text"
+						labelText="job location"
+						name="jobLocation"
+						defaultValue={job.jobLocation}
+					/>
+					<FormRowSelect
+						labelText="job status"
+						name="jobStatus"
+						defaultValue={job.jobStatus}
+						list={Object.values(JOB_STATUS)}
+					/>
+					<FormRowSelect
+						name="jobType"
+						labelText="job type"
+						defaultValue={job.jobType}
+						list={Object.values(JOB_TYPE)}
+					/>
+					<FormRow
+						type="text"
+						name="jobURL"
+						labelText="Job URL"
+						defaultValue={job.jobURL || "No URL provided"}
+					/>
+					<FormRow
+						type="textarea" // Use textarea for job description
+						name="jobDescription"
+						labelText="Job Description"
+						defaultValue={job.jobDescription || "No description available"}
+					/>
+					<SubmitBtn formBtn />
+				</div>
+			</Form>
+		</Wrapper>
+	)
 }
 
 export default EditJob
